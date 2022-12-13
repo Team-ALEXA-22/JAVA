668d562625788a808ab16c4dc0718f69cf082dde
Team ALEXA 22 <teamalexa22@gmail.com>
2022-12-13 10:23:55 +0530

    Inheritance

---
+++ inheritance/src/inheritance/Inheritance.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author ABINESH JAIN A J
+ */
+public class Inheritance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        child dora=new child();
+        dora.name="Dora";
+        dora.gender="female";
+        dora.Name="Papa";
+        dora.Details();
+        dora.details();
+    }  
+}
---
+++ inheritance/src/inheritance/child.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author ABINESH JAIN A J
+ */
+class parent{
+    String Name;
+    void Details(){
+        System.out.println(Name+" ia a parent.");
+    }
+}
+public class child extends parent{
+    String name;
+    String gender;
+    void details(){
+        System.out.println(name+" is a "+gender);
+    }
+}
